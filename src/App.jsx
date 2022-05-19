5f03afa6c332f7e6c1b8ad882db330e779f47e6f
Alen-Berg <alexei.hlatsko@gmail.com>
2022-05-19 10:44:14 +0200

    links to registered users

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import { Routes, Route, Navigate} from 'react-router-dom';
 import { privateRoutes, publicRoutes } from './routes';
 import {HOME_ROUTE, PROFILE_ROUTE} from './utils/consts';
+import Layout from './components/Layout';
 
 function App() {
-  const user = true;
+  const user = false;
   return user ? 
     (
       <Routes>
-        {privateRoutes.map(({path, Component})=>
+        <Route path='/' element={<Layout />}>
+          {privateRoutes.map(({path, Component})=>
           <Route path={path} element={Component}></Route>
-        )}
-        <Route path="*" element={<Navigate to={PROFILE_ROUTE}/>}/>
+          )}
+          <Route path="*" element={<Navigate to={PROFILE_ROUTE}/>}/>
+        </Route>
       </Routes>
     )
     :
     (
       <Routes>
-        {publicRoutes.map(({path, Component})=>
-          <Route path={path} element={Component}></Route>
-        )}
-        <Route path="*" element={<Navigate to={HOME_ROUTE}/>}/>
+        <Route path='/' element={<Layout />}>
+          {publicRoutes.map(({path, Component})=>
+            <Route path={path} element={Component}></Route>
+          )}
+          <Route path="*" element={<Navigate to={HOME_ROUTE}/>}/>
+        </Route>
       </Routes>
     )}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// Initialize Firebase
+firebase.initializeApp(
+  {
+    apiKey: "AIzaSyDyrqQolmzA4sCkvlPJXXdhQisUUL26EPY",
+    authDomain: "freex-e983a.firebaseapp.com",
+    projectId: "freex-e983a",
+    storageBucket: "freex-e983a.appspot.com",
+    messagingSenderId: "561091024495",
+    appId: "1:561091024495:web:fbe2759361dde714349f9a",
+    measurementId: "G-8595E9L5KR"
+  }
+);
+
+const Context = createContext(null) 
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Context.Provider value={{
+        firebase,
+        auth,
+        firestore
+      }}>
+        <App />
+      </Context.Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
